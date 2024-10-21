6c8aa3c7094e4c547ea31e029415d56a9ee694f8
Divyesh-Thakur45 <divyeshthakur370@gmail.com>
2024-10-21 22:43:22 +0530

    update

--- src/Components/Contact.jsx
+++ src/Components/Contact.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-
+import "../Style/contact.css";
 const Contact = () => {
   return (
     <div>Contact</div>
--- src/Components/Projects.jsx
+++ src/Components/Projects.jsx
@@ -5,17 +5,41 @@ import lenskart from "../Images/LenskartMokeUp.jpg";
 const Projects = () => {
   return (
     <div className="project-page">
-      <h1 className="text-3xl font-bold mb-4">Projects</h1>
+       <div className="Skill-page-header">
+        <h1>Projects</h1>
+        <span className="line-1"></span>
+        <span className="line-2"></span>
+      </div>
 
-      <div className="Box">
-        <img src={lenskart} alt="Project 1" className="z-10 relative" />
+      <div className="timeLine">
+        <div className="main-box">
+          <span class="goll1"></span>
+          <span class="line1"></span>
+          <div className="Box">
+            <img src={lenskart} alt="Project 1" className="z-10 relative" />
 
-        <h2 className="text-2xl font-semibold z-10 relative">
-          Project Title 1
-        </h2>
-        <p className="mt-2 text-gray-600 z-10 relative">
-          Description of the project 1
-        </p>
+            <h2 className="text-2xl font-semibold z-10 relative">
+            Lenskart Clone
+            </h2>
+            <p className="mt-2 text-gray-600 z-10 relative">
+              Description of the project 1
+            </p>
+          </div>
+        </div>
+        <div className="main-box2">
+          <span class="goll2"></span>
+          <span class="line2"></span>
+          <div className="Box">
+            <img src={lenskart} alt="Project 1" className="z-10 relative" />
+
+            <h2 className="text-2xl font-semibold z-10 relative">
+              Project Title 1
+            </h2>
+            <p className="mt-2 text-gray-600 z-10 relative">
+              Description of the project 1
+            </p>
+          </div>
+        </div>
       </div>
     </div>
   );
