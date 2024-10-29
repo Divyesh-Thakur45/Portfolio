7bfa667549b374718d8e5ff53940018504a39ae3
Divyesh-Thakur45 <divyeshthakur370@gmail.com>
2024-10-29 13:04:24 +0530

    update

--- src/Components/Projects.jsx
+++ src/Components/Projects.jsx
@@ -1,11 +1,14 @@
-import React from "react";
+import React, { useContext } from "react";
 import "../Style/Project.css";
 import lenskart from "../Images/LenskartMokeUp.jpg";
+import Amazone from "../Images/amazonMockUp.jpg";
+import { themeContext } from "./Context/Allcontext";
 
 const Projects = () => {
+  const { Theme } = useContext(themeContext);
   return (
-    <div className="project-page">
-       <div className="Skill-page-header">
+    <div className={`project-page ${Theme ? `bg-[#000000]` : `bg-[#ECF0F3]`}`}>
+      <div className="Skill-page-header">
         <h1>Projects</h1>
         <span className="line-1"></span>
         <span className="line-2"></span>
@@ -19,10 +22,99 @@ const Projects = () => {
             <img src={lenskart} alt="Project 1" className="z-10 relative" />
 
             <h2 className="text-2xl font-semibold z-10 relative">
-            Lenskart Clone
+              Lenskart Clone
             </h2>
-            <p className="mt-2 text-gray-600 z-10 relative">
-              Description of the project 1
+            <p className="z-10 relative flex justify-evenly mt-5">
+              <a
+                href="/"
+                class="button button--nina px-5 py-0 bg-gray-800 hover:bg-gray-700 text-gray-300 hover:text-white relative block focus:outline-none border-2 border-solid rounded-lg text-sm text-center font-semibold uppercase tracking-widest overflow-hidden"
+                data-text="Live"
+              >
+                <span class="align-middle">L</span>
+                <span class="align-middle">i</span>
+                <span class="align-middle">v</span>
+                <span class="align-middle">e</span>
+              </a>
+              <a
+                href="/"
+                class="button button--nina px-5 py-0 bg-gray-800 hover:bg-gray-700 text-gray-300 hover:text-white relative block focus:outline-none border-2 border-solid rounded-lg text-sm text-center font-semibold uppercase tracking-widest overflow-hidden"
+                data-text="Code"
+              >
+                <span class="align-middle">C</span>
+                <span class="align-middle">o</span>
+                <span class="align-middle">d</span>
+                <span class="align-middle">e</span>
+              </a>
+
+            </p>
+
+          </div>
+        </div>
+        <div className="main-box2">
+          <span class="goll2"></span>
+          <span class="line2"></span>
+          <div className="Box">
+            <img src={Amazone} alt="Project 1" className="z-10 relative" />
+
+            <h2 className="text-2xl font-semibold z-10 relative">
+              Amazone Clone
+            </h2>
+            <p className="z-10 relative flex justify-evenly mt-5">
+              <a
+                href="/"
+                class="button button--nina px-5 py-0 bg-gray-800 hover:bg-gray-700 text-gray-300 hover:text-white relative block focus:outline-none border-2 border-solid rounded-lg text-sm text-center font-semibold uppercase tracking-widest overflow-hidden"
+                data-text="Live"
+              >
+                <span class="align-middle">L</span>
+                <span class="align-middle">i</span>
+                <span class="align-middle">v</span>
+                <span class="align-middle">e</span>
+              </a>
+              <a
+                href="/"
+                class="button button--nina px-5 py-0 bg-gray-800 hover:bg-gray-700 text-gray-300 hover:text-white relative block focus:outline-none border-2 border-solid rounded-lg text-sm text-center font-semibold uppercase tracking-widest overflow-hidden"
+                data-text="Code"
+              >
+                <span class="align-middle">C</span>
+                <span class="align-middle">o</span>
+                <span class="align-middle">d</span>
+                <span class="align-middle">e</span>
+              </a>
+
+            </p>
+          </div>
+        </div>
+        <div className="main-box">
+          <span class="goll1"></span>
+          <span class="line1"></span>
+          <div className="Box">
+            <img src={lenskart} alt="Project 1" className="z-10 relative" />
+
+            <h2 className="text-2xl font-semibold z-10 relative">
+              BrookStone Clone
+            </h2>
+            <p className="z-10 relative flex justify-evenly mt-5">
+              <a
+                href="/"
+                class="button button--nina px-5 py-0 bg-gray-800 hover:bg-gray-700 text-gray-300 hover:text-white relative block focus:outline-none border-2 border-solid rounded-lg text-sm text-center font-semibold uppercase tracking-widest overflow-hidden"
+                data-text="Live"
+              >
+                <span class="align-middle">L</span>
+                <span class="align-middle">i</span>
+                <span class="align-middle">v</span>
+                <span class="align-middle">e</span>
+              </a>
+              <a
+                href="/"
+                class="button button--nina px-5 py-0 bg-gray-800 hover:bg-gray-700 text-gray-300 hover:text-white relative block focus:outline-none border-2 border-solid rounded-lg text-sm text-center font-semibold uppercase tracking-widest overflow-hidden"
+                data-text="Code"
+              >
+                <span class="align-middle">C</span>
+                <span class="align-middle">o</span>
+                <span class="align-middle">d</span>
+                <span class="align-middle">e</span>
+              </a>
+
             </p>
           </div>
         </div>
@@ -33,10 +125,30 @@ const Projects = () => {
             <img src={lenskart} alt="Project 1" className="z-10 relative" />
 
             <h2 className="text-2xl font-semibold z-10 relative">
-              Project Title 1
+              Calculator
             </h2>
-            <p className="mt-2 text-gray-600 z-10 relative">
-              Description of the project 1
+            <p className="z-10 relative flex justify-evenly mt-5">
+              <a
+                href="/"
+                class="button button--nina px-5 py-0 bg-gray-800 hover:bg-gray-700 text-gray-300 hover:text-white relative block focus:outline-none border-2 border-solid rounded-lg text-sm text-center font-semibold uppercase tracking-widest overflow-hidden"
+                data-text="Live"
+              >
+                <span class="align-middle">L</span>
+                <span class="align-middle">i</span>
+                <span class="align-middle">v</span>
+                <span class="align-middle">e</span>
+              </a>
+              <a
+                href="/"
+                class="button button--nina px-5 py-0 bg-gray-800 hover:bg-gray-700 text-gray-300 hover:text-white relative block focus:outline-none border-2 border-solid rounded-lg text-sm text-center font-semibold uppercase tracking-widest overflow-hidden"
+                data-text="Code"
+              >
+                <span class="align-middle">C</span>
+                <span class="align-middle">o</span>
+                <span class="align-middle">d</span>
+                <span class="align-middle">e</span>
+              </a>
+
             </p>
           </div>
         </div>
